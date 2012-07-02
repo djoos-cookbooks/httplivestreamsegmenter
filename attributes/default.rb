40af960c70ed450bbea50de4f6d75ecaa9e47aef
David Joos <david.joos@gmail.com>
2012-07-02 12:54:39 +0100

    default git repo changed: VRT-medialab > VRT-onderzoek-en-innovatie

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,6 +6,6 @@
 #
 
 default[:httplivestreamsegmenter][:prefix] = "/usr/local"
-default[:httplivestreamsegmenter][:git_repository] = "git://github.com/VRT-medialab/HTTP-Live-Stream-Segmenter.git"
+default[:httplivestreamsegmenter][:git_repository] = "git://github.com/VRT-onderzoek-en-innovatie/HTTP-Live-Stream-Segmenter.git"
 default[:httplivestreamsegmenter][:git_revision] = "HEAD"
 default[:httplivestreamsegmenter][:compile_flags] = []
