b467d443c23349dfc5af3dace7100ddb034fcaf2
David Joos <david.joos@gmail.com>
2014-08-04 20:14:12 +0100

    Syntax/lint checks / unit tests / integration tests

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,10 +2,10 @@
 # Cookbook Name:: httplivestreamsegmenter
 # Attributes:: default
 #
-# Copyright 2012-2013, Escape Studios
+# Copyright 2012-2014, Escape Studios
 #
 
-default[:httplivestreamsegmenter][:prefix] = "/usr/local"
-default[:httplivestreamsegmenter][:git_repository] = "git://github.com/VRT-onderzoek-en-innovatie/HTTP-Live-Stream-Segmenter.git"
-default[:httplivestreamsegmenter][:git_revision] = "HEAD"
+default[:httplivestreamsegmenter][:prefix] = '/usr/local'
+default[:httplivestreamsegmenter][:git_repository] = 'git://github.com/VRT-onderzoek-en-innovatie/HTTP-Live-Stream-Segmenter.git'
+default[:httplivestreamsegmenter][:git_revision] = 'HEAD'
 default[:httplivestreamsegmenter][:compile_flags] = []
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,50 +2,50 @@
 # Cookbook Name:: httplivestreamsegmenter
 # Recipe:: default
 #
-# Copyright 2012-2013, Escape Studios
+# Copyright 2012-2014, Escape Studios
 #
 
-include_recipe "build-essential"
-include_recipe "git"
+include_recipe 'build-essential'
+include_recipe 'git'
 
-#libssl-dev (libcrypto)
-package "libssl-dev" do
+# libssl-dev (libcrypto)
+package 'libssl-dev' do
   action :upgrade
 end
 
 creates = "#{node[:httplivestreamsegmenter][:prefix]}/bin/MpegtsH264"
 
-file "#{creates}" do
-	action :nothing
+file creates do
+  action :nothing
 end
 
 git "#{Chef::Config[:file_cache_path]}/http-live-stream-segmenter" do
-	repository node[:httplivestreamsegmenter][:git_repository]
-	reference node[:httplivestreamsegmenter][:git_revision]
-	action :sync
-	notifies :delete, "file[#{creates}]", :immediately
+  repository node[:httplivestreamsegmenter][:git_repository]
+  reference node[:httplivestreamsegmenter][:git_revision]
+  action :sync
+  notifies :delete, "file[#{creates}]", :immediately
 end
 
-#write the flags used to compile to disk
+# write the flags used to compile to disk
 template "#{Chef::Config[:file_cache_path]}/httplivestreamsegmenter-compiled_with_flags" do
-	source "compiled_with_flags.erb"
-	owner "root"
-	group "root"
-	mode 0600
-	variables(
-		:compile_flags => node[:httplivestreamsegmenter][:compile_flags]
-	)
-	notifies :delete, "file[#{creates}]", :immediately
+  source 'compiled_with_flags.erb'
+  owner 'root'
+  group 'root'
+  mode 0600
+  variables(
+    :compile_flags => node[:httplivestreamsegmenter][:compile_flags]
+  )
+  notifies :delete, "file[#{creates}]", :immediately
 end
 
-bash "compile_httplivestreamsegmenter" do
-	cwd "#{Chef::Config[:file_cache_path]}/http-live-stream-segmenter"
-	code <<-EOH
-		touch README
-		autoreconf
-		automake --add-missing
-		./configure --prefix=#{node[:httplivestreamsegmenter][:prefix]} #{node[:httplivestreamsegmenter][:compile_flags].join(' ')}
-		make clean && make && make install
-	EOH
-	creates "#{creates}"
+bash 'compile_httplivestreamsegmenter' do
+  cwd "#{Chef::Config[:file_cache_path]}/http-live-stream-segmenter"
+  code <<-EOH
+    touch README
+    autoreconf
+    automake --add-missing
+    ./configure --prefix=#{node[:httplivestreamsegmenter][:prefix]} #{node[:httplivestreamsegmenter][:compile_flags].join(' ')}
+    make clean && make && make install
+  EOH
+  creates creates
 end
